db494855574f522b455068417b47d74c765372db
raghav-appscrip <raghav@appscrip.co>
2025-04-22 12:15:58 +0530

    Initial Commit

--- app/src/main/java/com/example/discovery/MainActivity.kt
+++ app/src/main/java/com/example/discovery/MainActivity.kt
@@ -5,12 +5,19 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -32,12 +39,28 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var tweetsApiService: TweetsyApi
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
         setContent {
-            App()
+            Scaffold (
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(text = "Tweetsy" , color = Color.White)
+                        } ,
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = Color.Black,
+                            titleContentColor = Color.White
+                        )
+                    )
+                }
+            ) {
+                Box(modifier = Modifier.padding(it)){
+                    App()
+                }
+            }
+
         }
     }
 }
